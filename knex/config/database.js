a677d6ab97bc33a2f32f7110b267b47d8f87a964
abreuitalo <italo.ab.leite@gmail.com>
2025-01-09 21:39:59 -0300

    knex

---
+++ knex/config/database.js
@@ -0,0 +1,6 @@
+const knexfile = require("../../knexfile");
+const knex = require("knex")(knexfile);
+
+knex("users").then((data) => console.log(data)); // SELECT * FROM users;
+
+module.exports = knex; // exportando knex
